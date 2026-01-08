a0348e58e545698c77c58aa109c1891268f86ac6
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-08 21:31:02 +0530

    oops.dart: oops?, oops in dart?, advantages, real world match, and pillars add.

---
+++ Basics/oops.dart
@@ -0,0 +1,27 @@
+/// OOPs : Object Oriented Programming
+
+// OOP is a programming paradigm (way of writing programs),
+// where we structure our code using classes and objects.
+
+/// OOPs in Dart
+// It is a pure Object Oriented language.
+// Everything in Dart is an object (int, double, String, function, etc.)
+// OOPs helps to model real-world entities and write clean, reusable, and scalable code.
+// Main Purpose : break complex problems into smaller objects
+
+/// Advantages
+// Organize large codebases
+// Reuse code
+// Improve readability
+// Maintain & scale Flutter apps easily
+
+/// In Real world
+// Car has properties → color, model, speed
+// Car has behaviors → start(), stop(), accelerate()
+// we represent this using class
+
+/// Pillars
+// Encapsulation
+// Inheritance
+// Abstraction
+// Polymorphism
