fa6fc834e6c915f9aa735b39e1e1c8e344d7bfba
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-09 16:25:30 +0530

    default_constructor.dart: default constructor?, example of default constructor.

---
+++ Basics/Constructor/default_constructor.dart
@@ -0,0 +1,16 @@
+/// Default Constructor
+// automatically created by dart compiler not by developer
+// className() : no parameter
+
+class School {
+  String? schoolName;
+  int? numOfStudents;
+
+  School() {
+    print('This is Default Constructor');
+  }
+}
+
+void main() {
+  School abcSchool = School(); // This is Default Constructor
+}
