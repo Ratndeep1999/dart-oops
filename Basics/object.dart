78cdb0475db063bd2dc76029fad2d6b6f0c254a8
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-09 10:38:49 +0530

    object.dart: object?, components, syntax.

---
+++ Basics/object.dart
@@ -0,0 +1,15 @@
+/// Object
+
+// An object is an instance of a class.
+// It is a real world entity.
+// Objects are created by using templates called classes.
+// An object made up of class members (properties and methods)
+
+/// Components
+// Object type : data type of the object
+// Object Name : it is a real world entity name
+// Class Constructor : use to initialize the object
+
+/// Syntax
+// objectType objectName = className();
+
