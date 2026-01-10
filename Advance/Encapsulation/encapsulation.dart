9c45c2eafbc7e79f881aebd10dbf444cc96f53c4
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-10 10:14:36 +0530

    encapsulation.dart: encapsulation?, how to achieve?.

---
+++ Advance/Encapsulation/encapsulation.dart
@@ -0,0 +1,9 @@
+/// Encapsulation : Fundamental representation of oops
+// It means hiding data within a library and restrict it from outside direct access.
+// Encapsulation provides shield for internal representation of object
+// to restrict outside interference, misuse and data leak.
+// Encapsulation achieve through access modifiers.
+
+/// Encapsulation achieved :
+// Making class members as private and providing public 'getter' and 'setter'
+// to access and update the value op private members.
