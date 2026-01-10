da14417e1f328420a8aac3151594fda8ff4170f1
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-10 09:27:24 +0530

    redirecting_constructor.dart: definition and example add

---
+++ Basics/Constructor/redirecting_constructor.dart
@@ -0,0 +1,30 @@
+/// Redirecting constructor
+// Redirecting constructors allow you to call another constructor from within a constructor.
+
+class Rectangle {
+  double width;
+  double height;
+
+  // Parameterized constructor
+  Rectangle({required this.width, required this.height});
+
+  // Redirection constructor
+  Rectangle.square(double size)
+    : this(width: size, height: size); // call main constructor
+
+  showData() {
+    print('Width: $width');
+    print('Height: $height');
+  }
+
+  findAres() => width * height;
+}
+
+void main() {
+  Rectangle square = Rectangle.square(5);
+  square.showData();
+  // Width: 5.0
+  // Height: 5.0
+  print('Area of Square: ${square.findAres()}');
+  // Area of Square: 25.0
+}
