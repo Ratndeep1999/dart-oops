1e9af7f548e054ca8971b78c57e2b45f2f1d6b4c
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-10 10:10:11 +0530

    public_and_private.dart: public, private and library fundamental add

---
+++ Advance/Encapsulation/public_and_private.dart
@@ -0,0 +1,9 @@
+/// Public Members
+// Class members that can be access in any dart file within project.
+
+/// Private Members
+// Class members that can accessible only inside the file where the class is present.
+
+/// Library
+// Every .dart file is library, it is a collection of function and classes.
+// Import library into  another library using keyword 'import'
