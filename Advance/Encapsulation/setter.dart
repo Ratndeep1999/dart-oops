018b41706dcf7b17dd87a1b05d2a728e3f01d464
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-11 16:48:47 +0530

    setter.dart: setter definition, syntax, and examples.

--- Advance/Encapsulation/setter.dart
+++ Advance/Encapsulation/setter.dart
@@ -1,33 +1,82 @@
-/// Getter
-// Getter is use to get/access/read values of class private properties.
-// It provide indirect access of class private properties.
+/// Setter
+// Setter is use to set/update/write values of class private properties.
+// indirect write access of object private properties.
 
 /// Syntax
-// returnType get propertyName{
-//   return _propertyName;
+// set propertyName(type parameter){
+//   this._privateProperty = parameter;
 // }
 
-/// Getter only
+/// Person Class
 class Person {
   String? _firstName; // No constructor or late so it must be nullable.
   int? _age;
 
   // setter's
   set firstName(String firstName) => _firstName = firstName;
+
   set age(int age) => _age = age;
 
-  // getter's
-  String? get firstName => _firstName;
-  int? get age => _age;
+  void showDetails() {
+    print('Name: $_firstName');
+    print('Age: $_age');
+  }
+}
+
+/// Product class
+class Product {
+  String? _productName;
+  double? _productPrice;
+
+  // setters
+  set productName(String? productName) =>
+      this._productName = (productName ?? 'No Product');
+
+  set productPrice(double? productPrice) {
+    if (_productPrice! >= 0.0 || _productPrice! <= 50) {
+      throw Exception("Price is in between 0.0 & 50.0");
+    }
+    this._productPrice = productPrice;
+  }
+
+  showData() {
+    print('Product name: $_productName');
+    print('Product price: $_productPrice');
+    // print('Product price: ${_productPrice ?? 0.0}');
+  }
 }
 
 void main() {
-  Person person = Person();
-  person.firstName = 'Sonali';
-  person.age = 26;
+  /// Person objects
+  Person person1 = Person();
+  person1.showDetails();
+  // Name: null
+  // Age: null
 
-  print('Name: ${person.firstName} ');
-  print('Age: ${person.age} ');
+  Person person2 = Person();
+  person2.firstName = 'Sonali';
+  person2.age = 26;
+  person2.showDetails();
   // Name: Sonali
   // Age: 26
+
+  /// Product objects
+  Product product1 = Product();
+  product1.showData();
+  // Product name: null
+  // Product price: null
+
+  Product product2 = Product();
+  product2._productName = 'Soap';
+  product2._productPrice = 50.0;
+  product2.showData();
+  // Product name: Soap
+  // Product price: 50.0
+
+  Product product3 = Product();
+  product3.productName = null;
+  product3._productPrice = null;
+  product3.showData();
+  // Product name: No Product
+  // Product price: null
 }
