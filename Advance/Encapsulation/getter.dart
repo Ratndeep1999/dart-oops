c4de062bcfe8ac10a563a2c2c22695971c8ec079
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-14 11:39:49 +0530

    getter.dart: Getter?, syntax, and examples.

--- Advance/Encapsulation/getter.dart
+++ Advance/Encapsulation/getter.dart
@@ -7,29 +7,63 @@
 //   return _propertyName;
 // }
 
-/// Getter only
+/// Person class
 class Person {
-  String? _firstName; // No constructor or late so it must be nullable.
-  int? _age;
+  String _firstName; // Non nullable, because constructor is in use
+  String _lastName;
+  int _age;
 
-  Person({String? firstName, int? age}) : _firstName = firstName, _age = age;
+  Person({
+    required String firstName,
+    required String lastName,
+    required int age,
+  }) : _firstName = firstName,
+       _lastName = lastName,
+       _age = age;
 
   // getter's
-  String? get firstName => _firstName;
-  int? get age => _age;
+  String get fullName => '$_firstName $_lastName';
+  int get age => _age;
+}
+
+/// Map Getter
+class Student{
+  String _name;
+  int _age;
+  String _gender;
+
+  Student(this._name, this._age, this._gender);
+
+  // getters
+ String get name => _name;
+ int get age => _age;
+ String get gender => _gender;
+
+ Map<String, dynamic> get studentMap{
+   return {
+     'Name': _name,
+     'Age': _age,
+     'Gender': _gender,
+   };
+ }
+
 }
 
 void main() {
-  Person person1 = Person();
+  /// Person object
+  Person person1 = Person(
+    firstName: 'Ratndeep',
+    lastName: 'Chandankhede',
+    age: 26,
+  );
 
-  print('Name: ${person1.firstName} ');
-  print('Age: ${person1.age} ');
-  // Name: null
-  // Age: null
+  String fullName = person1.fullName;
+  print('Full name: $fullName'); // Full name: Ratndeep Chandankhede
+  print('Age: ${person1.age}'); // Age: 26
 
-  Person person2 = Person(firstName: 'Sonali', age: 26);
-  print('Name: ${person2.firstName} ');
-  print('Age: ${person2.age} ');
-  // Name: Sonali
-  // Age: 26
+  /// Student object
+  Student s1 = Student('Ratndeep', 26, 'Male');
+  print('Name: ${s1.name}'); // Name: Ratndeep
+  print('Age: ${s1.studentMap}'); // Age: {Name: Ratndeep, Age: 26, Gender: Male}
+  print('Age: ${s1.studentMap['Age']}'); // Age: 26
 }
