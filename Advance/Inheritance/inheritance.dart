854d3e1b800ce3fff644704356c23fa349c93e6f
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-14 15:58:22 +0530

    inheritance.dart: inheritance?, IS-A relation, syntax, terminology, and benefits.

---
+++ Advance/Inheritance/inheritance.dart
@@ -0,0 +1,38 @@
+/// Inheritance: fundamental concept of oop's
+// It is the process in which one class acquires the properties of another class.
+// Keyword 'extends' is used to inherit the class in given class.
+// Inheritance allow to inherit properties and methods from parent to child class.
+
+/// Inheritance IS-A relation
+// It always create 'IS-A' relation between the parent and child class. like :
+// Student IS A Person :  Student is child class
+// Truck IS A Vehicle : Vehicle is parent class
+// Cow IS A Animal : Animal is parent class
+
+/// syntax
+// class SubClassName extends SuperClassName {
+//   // body
+// }
+
+/// Key-concepts or terminology
+/// Base/Parent/Super class:
+// class whose properties & methods are inherited by another class(child).
+
+/// Derive/Child/Sub class:
+// class that inherits properties & methods from base class,
+// it can add new properties & methods or override existing ones.
+
+/// Method Overriding:
+// Ability of child class to provide a specific implementation
+// of method that is already defined in its parent class.
+
+/// 'Super' keyword:
+// super is used to refer parent class,
+// it can be used to call parent constructor or methods.
+
+/// Note: Dart wont support multiple inheritance means class can not inherit from multiple classes.
+
+/// Benefits of Inheritance
+// Code reusability.
+// Design program in a better way.
+// make code simple, clean and save time and money on maintenance.
