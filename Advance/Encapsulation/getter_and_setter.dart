5f20d77083a1b1fdbd3fb61e88a652127e982e28
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-14 11:41:30 +0530

    getter_and_setter.dart: getter and setter both?, and why we use it.

---
+++ Advance/Encapsulation/getter_and_setter.dart
@@ -0,0 +1,10 @@
+/// Both getters & setters
+// Validate data before read and write private properties.
+// Performs some actions before reading & writing properties.
+// Both provides controlled access to an object properties.
+
+/// Why Use Getter and Setter
+// Help to hide internal representation of an object & expose only what is necessary.
+// Use validation to setters to ensure only valid data is assigned to properties.
+// Getter can be use to compute values dynamically base on other properties.
+
