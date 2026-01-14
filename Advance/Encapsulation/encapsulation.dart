4d6fdb9df0f20a955cfb1b3e5eb35215b9ccee69
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-14 11:38:57 +0530

    encapsulation.dart: encapsulation?, achieved by, and examples.

--- Advance/Encapsulation/encapsulation.dart
+++ Advance/Encapsulation/encapsulation.dart
@@ -1,43 +1,46 @@
 /// Encapsulation : Fundamental representation of oops
-// It means hiding data within a library and restrict it from outside direct access.
+// It represent hiding data within a library(.dart) and restrict direct access from outsiders.
 // Encapsulation provides shield for internal representation of object
 // to restrict outside interference, misuse and data leak.
 // Encapsulation achieve through access modifiers.
 
 /// Encapsulation achieved :
 // Making class members as private and providing public 'getter' and 'setter'
-// to access and update the value op private members.
+// to access and update the value of private members.
 // Getter : access & read.
-// Setter : update & set.
+// Setter : update & write.
 
 class Person {
   String? _name;
   int? _age;
 
+  // get method
   String? getName() => _name;
-
   int? getAge() => _age;
 
+  // set method
   void setName(String name) => _name = name;
-
   void setAge(int age) => _age = age;
 }
 
 /// Note:
 /// Problem 1
-// If properties is final then we can not change it latter
-// means we can set values at once before run if we use setter to set value
-// after run then ir return exception.
+// If properties is final then we can not change after run
+// If modify values after run then it return exception.
 
 /// Problem 2
 // If properties is not final means null or constructor is used
 // If constructor is use means values never null
-// If null use means getter can return null.
+// If null use means getters can be nullable
 
-/// Solution : to override
-// Use null and make getters nullable type and handel null operation inside print().
-// Use late but insure to initialize before use otherwise Unhandled exception.
-// If not null, final, and late means constructor is used.
+/// Problem 3
+// If late keyword is use then object properties must be initialize
+// before use otherwise throw exception
+
+/// Solution :
+// Use ? and make getters nullable type and handel null validation inside getters.
+// With fields ? not use, constructor not use but setter use so, it's not sufficient.
+// Use constructor, ? or late.
 
 class Student {
   String? _name;
@@ -53,19 +56,21 @@ class Student {
 }
 
 void main() {
+  /// Person object
   Person p1 = Person();
   p1.setName('Sonali');
   p1.setAge(26);
   print('Name: ${p1.getName()}'); // Name: Sonali
   print('Age: ${p1.getAge()}'); // Age: 26
 
+  /// Student object
   // Student s1 = Student(name: 'Ratndeep');
   // print('Name: ${s1._name}'); // Name: Ratndeep
-  // print('School name: ${s1._schoolName}'); // Name: ABC School
+  // print('School name: ${s1._schoolName}'); // School name: ABC School
   // s1.setSchoolName('XYZ School');
-  // print('School name: ${s1._schoolName}'); // Name: ABC School
+  // print('School name: ${s1._schoolName}'); // School name: XYZ School
 
   Student s2 = Student();
-  print('Name: ${s2._name ?? 'Sonali'}');
-  print('Name: ${s2._schoolName ?? 'XYZ School'}');
+  print('Name: ${s2._name ?? 'Sonali'}'); // Name: Sonali
+  print('School name: ${s2._schoolName ?? 'XYZ School'}'); // School name: XYZ School
 }
