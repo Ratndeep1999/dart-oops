16470cdb7494098512d1e496bfcaee7120b9eb34
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-15 17:31:30 +0530

    instance_variable.dart: instance variable?, key-points and eg add.

---
+++ Advance/Instance and Static Members/instance_variable.dart
@@ -0,0 +1,15 @@
+/// Instance Variable
+// Not-static variable declared within a class.
+// Belongs to an instance (object) of the class
+// means each object has its own copy of variable.
+
+/// Key-Points
+// Stores data/state to an object.
+// Have access modifiers (eg. public or private).
+// Initialize when object is created, often with default or constructor-provided values.
+
+class Person {
+  // Instance variable
+  String? name;
+  int? Age;
+}
