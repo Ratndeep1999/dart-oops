8ec3ab8e6374341519ec51328e4b93b34cba96d8
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-15 10:02:50 +0530

    with_named.dart: inheritance of named constructor add.

--- Advance/Inheritance/Inheritance of Constructor/with_named.dart
+++ Advance/Inheritance/Inheritance of Constructor/with_named.dart
@@ -4,32 +4,26 @@ class Animal {
 
   Animal({required this.numOfLegs, required this.isTale});
 
-  Animal.onlyLegs({required this.numOfLegs, this.isTale = false}) {
-    print('Animal Named Constructor');
+  Animal.onlyLegs({required this.numOfLegs, this.isTale = true}) {
+    print('Animal Named Constructor Invoke');
   }
 
   // another syntax
   // Animal.onlyLegs(this.numOfLegs) : this.isTale = false {
-  //   print('Animal Named Constructor');
+  //   print('Animal Named Constructor Invoke');
   // }
 }
 
 class Dog extends Animal {
   int lifeSpan;
 
-  Dog({required int numOfLegs, required bool isTale, required this.lifeSpan})
-    : super.onlyLegs(
-        numOfLegs: numOfLegs,
-      ); // I can put isTale: isTale, that affect true
+  Dog({required int numOfLegs, required this.lifeSpan})
+    : super.onlyLegs(numOfLegs: numOfLegs); // I can put isTale: isTale, that affect parent constructor
 }
 
 void main() {
-  Dog d1 = Dog(
-    numOfLegs: 4,
-    lifeSpan: 12,
-    isTale: true,
-  ); //Animal Named Constructor
+  Dog d1 = Dog(numOfLegs: 4, lifeSpan: 12); // Animal Named Constructor
   print('Total legs of Dog ${d1.numOfLegs}'); // Total legs of Dog 4
-  print('Is Dog has tail: ${d1.isTale}'); // Is Dog has tail: false
+  print('Is Dog has tail: ${d1.isTale}'); // Is Dog has tail: true
   print('Lifespan of Dog ${d1.lifeSpan}'); // Lifespan of Dog 12
 }
