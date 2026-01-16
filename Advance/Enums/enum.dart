f742b01308ac00d360a0a24ac00fbb1e131921c9
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-16 16:14:58 +0530

    enum.dart: enum? and syntax add

---
+++ Advance/Enums/enum.dart
@@ -0,0 +1,15 @@
+///enum:
+// Enum is a special type that represents fix number of constant values.
+// Declare using keyword 'enum'
+// It must contain at least one constant value.
+// Enums are declared outside the class.
+// Used to store a large number of constant values.
+
+/// syntax
+// enum enumName {
+//   constantValue1,
+//   constantValue2,
+//   constantValue3,
+//   ...
+//   constantValueN,
+// }
