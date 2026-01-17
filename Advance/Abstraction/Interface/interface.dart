1c9b65da0af5f5f8751ce2e86c0252abf1f3172a
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-17 16:46:55 +0530

    interface.dart: interface?, syntax, and keypoints add.

--- Advance/Abstraction/Interface/interface.dart
+++ Advance/Abstraction/Interface/interface.dart
@@ -1,10 +1,11 @@
 /// Interface:
 // In Dart, every class is implicit interface.
 // Interface provides a syntax that a class must follow.
+// We can use concrete or abstract class to declare an interface.
 // When we use implement an interface we must implement all properties
 // and methods that defines in the interface.
 // 'implements' keyword use to implement interface.
-// An Interface specifies a set of methods that a class must implement,
+// An Interface specifies a set of methods that a class must implement
 // without providing any implementation details.
 
 /// syntax
@@ -18,6 +19,5 @@ class ClassName implements InterfaceName {
 /// Key-Points
 // Interface is a contract that defines the capabilities of a class.
 // We can use abstract keyword to declare an interface.
-// sub-class can extend only one class but interface can implement multiple interfaces.
 // Using interface, I can achieve multiple inheritance.
-// Use to achieve abstraction.
+// Interface Use to achieve abstraction.
