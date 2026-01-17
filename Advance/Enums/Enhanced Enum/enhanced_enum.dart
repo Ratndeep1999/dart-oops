75a23c65b002ebec6725672109004ba3a1a70695
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-17 15:45:46 +0530

    enhanced_enum.dart: syntax of enhanced add.

---
+++ Advance/Enums/Enhanced Enum/enhanced_enum.dart
@@ -0,0 +1,12 @@
+/// syntax
+// enum enumName {
+//   value1(parameters),
+//   value2(parameters),
+//   ....
+//   valueN(parameters);
+//
+//   final type variable;
+//
+//   const EnumName (this.parameter);
+//   methods, getters or setters etc.
+// }
