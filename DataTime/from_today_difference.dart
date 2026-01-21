09033c64c121bedfcc6c531c074075a907353337
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-21 20:07:28 +0530

    from_today_difference.dart: program that show the duration difference in between two dates using DateTime object.

---
+++ DataTime/from_today_difference.dart
@@ -0,0 +1,26 @@
+/// Get total difference in between two dates
+
+void main() {
+  // get current data and time
+  DateTime toady = DateTime.now();
+
+  // convert birthday data to DateTime type
+  DateTime myBirthday = DateTime.parse('1999-02-06');
+
+  // get duration difference of two dates
+  Duration duration = toady.difference(myBirthday);
+
+  print('Completed Days till now: ${duration.inDays}');
+  print('Completed Hours: ${duration.inHours}');
+  print('Completed Minutes: ${duration.inMinutes}');
+  print('Completed Seconds: ${duration.inSeconds}');
+  print("Completed Milliseconds : ${duration.inMilliseconds}");
+  print("Completed Microseconds : ${duration.inMicroseconds}");
+
+  // Completed Days till now: 9846
+  // Completed Hours: 236324
+  // Completed Minutes: 14179443
+  // Completed Seconds: 850766628
+  // Completed Milliseconds : 850766628324
+  // Completed Microseconds : 850766628324959
+}
