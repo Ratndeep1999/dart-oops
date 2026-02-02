cf0f6d02b3505e591613e6f1c7b84fe10ac10ecd
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-02 19:50:45 +0530

    generics_variable_type.dart: Generics variable type, and their separate examples.

---
+++ Advance/Generics/generics_variable_type.dart
@@ -0,0 +1,48 @@
+/// Generics variable type
+// T --> Type (represents a single data type)
+// E --> Element (commonly used for collections [list, set])
+// K --> Key (use for map keys)
+// V --> Value (used for map values)
+
+/// T (Type)
+class Box<T> {
+  T item;
+
+  Box(this.item);
+}
+
+/// E (Element)
+class MyList<E> {
+  List<E> items = [];
+
+  void add(E element) {
+    items.add(element);
+  }
+}
+
+/// K and V (Key-Value)
+class Pair<K, V> {
+  K key;
+  V value;
+
+  Pair(this.key, this.value);
+}
+
+void main() {
+  // T (Type)
+  Box<int> intBox = Box(10);
+  Box<String> stringBox = Box("Dart");
+  print(intBox.item); // 10
+  print(stringBox.item); // Dart
+
+  // E (Element)
+  MyList<String> myList = MyList();
+  myList.add('hello');
+  myList.add('world');
+  print(myList.items); // [hello, world]
+
+  // K and V (Key-Value)
+  Pair<int, String> user = Pair(1, "Ratndeep");
+  print(user.key); // 1
+  print(user.value); // Ratndeep
+}
