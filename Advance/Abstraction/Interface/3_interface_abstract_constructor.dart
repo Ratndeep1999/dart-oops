1de4a6157e9ffcd00637ee31ff5d2931724e94ca
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-05 16:33:25 +0530

    3_interface_abstract_constructor.dart: key points and definition maked shorter.

--- Advance/Abstraction/Interface/3_interface_abstract_constructor.dart
+++ Advance/Abstraction/Interface/3_interface_abstract_constructor.dart
@@ -12,6 +12,7 @@ abstract class Person {
 class Student implements Person {
   @override
   final String name;
+
   @override
   final int age;
 
@@ -48,22 +49,24 @@ void main() {
 }
 
 /// Case : abstract class + implements (Constructor NOT USED)
-// Here, Person’s constructor is ignored completely.
-// Because implements treats Person as a pure interface.
-// Student() : super(); // ERROR  ..So this is NOT allowed:
-// Student({required this.name, required this.age});
-// means constructor in Person exists because of abstract class,
-// but Student cannot access it because after 'implement' Student get Person as pure interface.
+// Here, Person’s constructor is ignored completely. Because implements treats Person as a pure interface.
+// Student() : super(); // ERROR  ..So this is NOT allowed
+// Student({required this.name, required this.age}); means constructor in Person exists because
+// of abstract class, but Student cannot access it because after 'implement' Student get Person
+// as pure interface.
 
-/// Important Rule: constructor + interface behavior not work together
-// Reuse constructor + fields --> use with 'extends'
-// Pure contract / interface  --> use with 'implements'
+/// Remember
+// When an abstract class is used with 'implements'
+// --> It behaves as a pure interface
+// --> Constructors are NOT inherited
+// --> 'super() cannot be called
+// --> All methods and getters must be implemented
+
+/// Rule:
+// Reuse constructor + state --> use 'extends'
+// Enforce behavior only --> use 'implements'
 
 /// Note:
-// When using an abstract class as an interface,
-// never declare fields directly — use getters instead. eg. String get name;
-// This is how repositories, entities, and domain layers are written in Flutter.
-
-// In Dart, when an abstract class is used as an interface via implements,
-// it should expose behavior and data through getters, not fields.
-// Constructors and state belong to the implementing class.
+// When using an abstract class as an interface, expose data using getters
+// (e.g. 'String get name;'), not fields. Constructors and state belong to
+// the implementing class (common in Flutter Clean Architecture).
