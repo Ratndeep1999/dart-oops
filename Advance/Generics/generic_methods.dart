0f6c3f0b603dcde4039f970d8c077a717d032154
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-05 21:22:31 +0530

    generic_methods.dart: comments change and line of space add.

--- Advance/Generics/generic_methods.dart
+++ Advance/Generics/generic_methods.dart
@@ -7,13 +7,13 @@ T methodName<T>(T value) {
   return value;
 }
 
-/// Example with single parameter
+/// Example of Generic method with a single parameter
 T printValue<T>(T value) {
   print(value);
   return value;
 }
 
-/// Example with multiple parameters
+/// Example of Generic method with multiple type parameters
 T printMultiValues<T, U>(T value1, U value2) {
   print('value1: $value1 & value2: $value2');
   return value1;
@@ -28,20 +28,18 @@ class Utils {
 }
 
 void main() {
-  /// Example with single parameters
+
+  /// Single parameters generic method
   printValue<int>(10); // 10
   printValue<String>("Dart"); // Dart
   printValue<double>(3.14); // 3.14
 
-  /// Example with multi parameters
-  printMultiValues<int, String>(
-    26,
-    "Ratndeep",
-  ); // value1: 26 & value2: Ratndeep
+  /// Multiple type parameters
+  printMultiValues<int, String>(26, "Ratndeep",); // value1: 26 & value2: Ratndeep
   printMultiValues<String, int>("Sonali", 25); // value1: Sonali & value2: 25
   printMultiValues<num, bool>(26.3, true); // value1: 26.3 & value2: true
 
-  /// Example with generic class method
+  /// Generic method inside a class
   Utils utils = Utils();
 
   print(utils.getFirst<int>([1, 2, 3])); // 1
