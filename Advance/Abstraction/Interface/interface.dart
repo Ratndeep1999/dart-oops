69f4985942bfdafffc58f3593cf4d312f9213051
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-05 16:18:39 +0530

    interface.dart: definition maked shorter and key points change.

--- Advance/Abstraction/Interface/interface.dart
+++ Advance/Abstraction/Interface/interface.dart
@@ -1,23 +1,25 @@
 /// Interface:
-// In Dart, every class is implicit interface.
-// Interface provides a syntax that a class must follow.
-// We can use concrete or abstract class to declare an interface.
-// When we use implement an interface we must implement all properties
-// and methods that defines in the interface.
-// 'implements' keyword use to implement interface.
-// An Interface specifies a set of methods that a class must implement
-// without providing any implementation details.
+// In Dart, every class can be used as an interface. An interface
+// provides a syntax that a class must follow. It specifies what
+// methods and properties a class must implement, without providing
+// implementation details.
+
+// We use 'implements' keyword to implement an interface. When a
+// class implements an interface, it must override and provide
+// implementations for all methods and properties.
+
+// Interfaces help achieve abstraction and loose coupling
 
 /// syntax
 class InterfaceName {
-  // body
+  // method and property declarations
 }
 class ClassName implements InterfaceName {
-  // body
+  // must implement all members of InterfaceName
 }
 
 /// Key-Points
 // Interface is a contract that defines the capabilities of a class.
-// We can use abstract keyword to declare an interface.
-// Using interface, I can achieve multiple inheritance.
-// Interface Use to achieve abstraction.
+// Interfaces can be declared using concrete or abstract classes.
+// Using interfaces, Dart supports multiple inheritance of contracts.
+// Interfaces are widely used in clean architecture and SOLID principles.
