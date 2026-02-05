8e0f218ca56046cf1f036fde42cb70d3164c2cbd
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-05 16:01:18 +0530

    abstraction.dart: definition make to short and key points add.

--- Advance/Abstraction/abstraction.dart
+++ Advance/Abstraction/abstraction.dart
@@ -1,28 +1,30 @@
 /// Abstraction : fundamental concept of oops
-// Abstraction is achieved through the use of abstract classes and interfaces.
-// Abstraction means hiding internal implementation of details and showing only what is necessary to the user.
-// Use to define behavior of class (abstract) that can be inherited by other classed.
-// Abstraction hides implementation details and exposes only essential behavior using abstract classes or interfaces.
-// Focus on what an object does, not how it does it.
+// Abstraction means hiding implementation details and showing
+// only essential features to the user. Focuses on what an
+// object does, not how it does it.
+
+// In dart, abstraction is achieved using
+// Abstract classes and Interfaces (implements).
 
 /// Key-Components
+
 // Abstract class:
-// Used to define the behavior of a class that can be inherited by other classes.
-// Declared using 'abstract' keyword before class keyword.
+// Declared using the 'abstract' keyword.
+// Cannot be instantiated directly.
 // We can not create an object of abstract class.
-// Class that cannot be instantiated, only intended to be subclassed.
+// Intended to be subclassed.
 // It contains both abstract methods (without body), concrete methods (with body), variables & constructors.
 
 // Abstract method:
-// Declared only inside abstract class, that do not have body.
-// Child must implement abstract methods (extends or implements).
+// Declared inside abstract classes or interfaces.
+// Does not have a method body.
+// Child classes must implement abstract methods.
 // Supports polymorphism.
 
 // Interfaces (implements):
-// In Dart, every class can be used as an interface.
-// When we implement an interface, we must implement all the properties and methods defined in the interface.
-// Keyword implements is used to implement an interface.
-// A class can implement multiple interfaces, allowing for flexible design.
+// In Dart, every class can act as an interface.
+// 'implements' forces the child class to implement all methods and properties.
+// A class can implement multiple interfaces.
 
 /// syntax
 // abstract class ClassName {
@@ -34,8 +36,8 @@
 
 /// Real life examples
 // ATM Machine:
-// We see: Withdraw, Deposit, Check Balance
-// We don’t see: database queries, network calls, security logic
+// We see: Withdraw(), Deposit(), Check Balance()
+// Hidden: database logic, networking, security logic.
 
 /// Use Abstraction in Flutter
 // API layers
@@ -45,4 +47,6 @@
 // Platform-specific logic
 // Clean Architecture
 
-/// Note: Not compulsory to implement concrete method of abstract class.
+// Note
+// Concrete methods are optional to override.
+// Abstract methods must be implemented.
