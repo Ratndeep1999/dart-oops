ec5b3668ffab8c0e07d73bdd0b6b2b318ded8f57
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-05 17:04:39 +0530

    just_after_interface.dart: restructure and write properly.

--- Advance/Abstraction/Interface/After Interface/just_after_interface.dart
+++ Advance/Abstraction/Interface/After Interface/just_after_interface.dart
@@ -1,49 +1,48 @@
-/// Some key-points of interface
-// Any class can act as an interface.
-// Constructors DO exist in that class but when we implements a class:
-// Constructor is NOT inherited and Variables are NOT inherited.
-// Only the contract is enforced means they are ignored by implements.
+/// key-points of interface
+// Any class can act as an interface. when a class is used with 'implements'.
+// Constructors and Variables (state) are not inherited, only method and property
 
-/// Then WHY are variables allowed in an interface?
+
+/// Why variables are allowed in interfaces ?
+
+// They are not real stored variables.
+// They define a contract for data access.
+
+// Example
 abstract class Animal {
   int get numberOfLegs;
 }
 
-// Here,
-// Not a stored variable, It is a property contract.
-// Variables in interfaces are NOT state.
-// Interface variables define WHAT data must be exposed. (like A promise, shape, public API)
+// This does not store data. It only promises that any implementing class
+// must expose 'numberOfLegs'.
 
-// How Dart treats interface variables internally
-// we write :
-// abstract class User {
-//   String name;
-// }
-
-// Dart actually treats :
+/// How Dart treats interface variables internally
 abstract class User {
   String get name;
 
   set name(String value);
 }
+// This means
+// --> Interface defines WHAT data must be available
+// --> Implementation decides HOW data is stored
 
 /// Why constructors are NOT allowed in interfaces (conceptually)
-// Constructors : Initialize state, Allocate memory and Define how object is created.
-// Interfaces: Do NOT care how object is created & about internal state
-// only care about what must exist
 
-// So It means allowing constructors in an interface would:
-// Break abstraction
-// Leak implementation details
+// Constructors : Initialize state, Allocate memory and Define how object is created.
+
+// Interfaces: Do not care how object is created & about internal state
+// only care about what must exist.
+
+// So It means allowing constructors in an interface would Break abstraction and Leak implementation details
 
 /// Interface = Structure + Behavior (NOT only structure)
-// “Is interface structure only?” NO
-// Interface defines:
+// “Is interface structure only?” no, Interface defines:
 // Behavior --> methods
-// Data access rules --> getters/setters
-// NOT Defines: Storage, Initialization, & Business logic.
+// Data access rules --> getters / setters
+// Not Defines: Storage, Initialization, & Business logic.
 
-// Real Flutter Example (ADVANCED)
+/// Real Flutter Example (ADVANCED)
+
 // Domain Layer:
 abstract class UserEntity {
   String get id; // an abstract class as an interface, use getters.
@@ -62,9 +61,9 @@ class FirebaseUserModel implements UserEntity {
 }
 
 /// Note:
-// Interface defines what?
-// Implementation defines how?
+// Interface defines what ?
+// Implementation defines how ?
 // Constructor exists only in implementation
 
 // This is how repositories, entities, and domain layers are written in Flutter.
-/// This is enterprise Flutter architecture.
+// This is enterprise Flutter architecture.
