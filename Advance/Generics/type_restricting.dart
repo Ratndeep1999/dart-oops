ae6977c1ce74e323f1bf9f95b58da1f59b1d84bf
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-05 21:16:50 +0530

    type_restricting.dart: comments add and definition rechanged.

--- Advance/Generics/type_restricting.dart
+++ Advance/Generics/type_restricting.dart
@@ -1,34 +1,36 @@
 /// Generic Type Restricting
-// Generic type restricting allows us to limit
-// the types that can be used with a generic.
+// Generic type restricting allows us to limit which types can be
+// used with a generic parameter. This is done using the `extends` keyword.
 
-/// Example of type restricting of class
-// Define generic class with bounded type
+/// Example of type restricting in a class
+// Only types that extend 'num' are allowed (int, double)
 class Data<T extends num> {
   T data;
 
   Data(this.data);
 }
 
-/// Example of type restricting of function
-// Define generic method
+/// Example of type restricting in a function
+// Generic method restricted to numeric types
 double getAverage<T extends num>(T value1, T value2) {
   return (value1 + value2) / 2;
 }
 
 void main() {
-  /// Example of type restricting in class
+  /// Type restricting in class
 
-  // create an object of type int and double
+  // Valid types - create an object of type int and double
   Data<int> intData = Data<int>(10);
   Data<double> doubleData = Data<double>(10.5);
+
   // print the data
   print("IntData: ${intData.data}"); // IntData: 10
   print("DoubleData: ${doubleData.data}"); // DoubleData: 10.5
-  // Not Possible
+
+  // Not Possible (String does not extend num)
   // Data<String> stringData = Data<String>("Hello");
 
-  /// Example of type restricting in function
+  /// Type restricting in function
 
   // call the generic method
   print("Average of int: ${getAverage<int>(10, 20)}"); // Average of int: 15.0
