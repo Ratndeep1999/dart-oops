1c0ff291c49dcf8f4e461ab1c50547e9d06f4480
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-05 20:41:16 +0530

    factory_constructor.dart: key points and definition change.

--- Advance/Factory Constructor/factory_constructor.dart
+++ Advance/Factory Constructor/factory_constructor.dart
@@ -1,49 +1,48 @@
 /// Factory Constructor
-// All constructor that i used previously are called as Generative Constructor
+// A factory constructor does not always create a new object.
+// Instead, it controls how and when an object is returned.
 
-/// What is Factory Constructor
-// A factory constructor does NOT always create a new object.
-// Instead, it controls how and when an object is created.
+// All constructor used earlier are called Generative Constructor
+
 
 /// Key Differences
 // Generative
 // --> Always create a new instance of class.
 
 // Factory
-// --> It can return
+// --> May return
 //     A new instance.
 //     An existing (cached) instance.
 //     An instance of a subclass.
 
-/// Mostly used for
+/// Commonly used for
 // To implement singleton or caching.
 // To return different objects based on logic.
-// To control object creation.
-// To hide complex initialization logic.
+// To hide complex object creation logic.
 
 /// Syntax
 // class ClassName {
 //   factory ClassName() {
-//     return ClassName Instance
+//     return ClassName._internal();
 //   }
 
 //   factory ClassName.namedConstructor() {
-//     return ClassName instance
+//     return ClassName._internal();
 //   }
+//
+//   ClassName._internal();
 // }
 
 /// Rules for creating factory constructors
-// 1. A factory constructor must return an instance of the same class
-// or one of its subclasses.
+// 1. Must return an instance of the same class or its subclass.
 
 // 2. Can’t use this keyword inside factory constructor. because
 // it does not directly create an object.
 
-// 3. It can be named or unnamed and called like normal constructor.
+// 3. Can be named or unnamed and called like normal constructor.
 
-// 4. It may return: new instance, cached (existing) instance,
-// or subclass instance.
+// 4. It may return: new instance, cached (existing) instance, or subclass instance.
 
 /// Note
-// With a factory, we can't initialize a final variables
-// using complex logic that can’t be handled in the initializer list.
+// Can initialize 'final' variables nut only through another constructor.
+// Cannot use an initializer list directly.
