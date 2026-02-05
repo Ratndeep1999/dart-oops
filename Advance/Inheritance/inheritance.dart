41d03a06615fb8474044181d16bb4d200d1db9f7
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-05 14:29:37 +0530

    inheritance.dart: Definition change and note add.

--- Advance/Inheritance/inheritance.dart
+++ Advance/Inheritance/inheritance.dart
@@ -1,38 +1,43 @@
 /// Inheritance: fundamental concept of oop's
-// It is the process in which one class acquires the properties of another class.
-// Keyword 'extends' is used to inherit the class in given class.
-// Inheritance allow to inherit properties and methods from parent to child class.
+// Inheritance is the process in which one class acquires the properties
+// and methods of another class. in given class. The 'extends' keyword is
+// used to inherit a class. Inheritance allows code reuse and logical hierarchy.
 
 /// Inheritance IS-A relation
-// It always create 'IS-A' relation between the parent and child class. like :
-// Student IS A Person :  Student is child class
-// Truck IS A Vehicle : Vehicle is parent class
-// Cow IS A Animal : Animal is parent class
+// Inheritance follows IS-A relationship
+// Student IS A Person (Student = child, Person = parent)
+// Truck IS A Vehicle (Truck = child, Vehicle = parent)
+// Cow IS A Animal (Cow = child, Animal = parent)
 
 /// syntax
 // class SubClassName extends SuperClassName {
 //   // body
 // }
 
-/// Key-concepts or terminology
-/// Base/Parent/Super class:
+/// Key terminology
+
+// Base / Parent / Super class:
 // class whose properties & methods are inherited by another class(child).
 
-/// Derive/Child/Sub class:
-// class that inherits properties & methods from base class,
-// it can add new properties & methods or override existing ones.
+// Derive / Child / Sub class:
+// class that inherits properties & methods from parent class,
+// it can add new members or override existing ones.
 
 /// Method Overriding:
-// Ability of child class to provide a specific implementation
-// of method that is already defined in its parent class.
+// When a child class provides its own implementation of a method
+// already defined in the parent class. In Dart, @override annotation is used.
 
 /// 'Super' keyword:
-// super is used to refer parent class,
-// it can be used to call parent constructor or methods.
+// super is used to refer to the immediate parent class
+// and access its constructor or methods.
 
-/// Note: Dart wont support multiple inheritance means class can not inherit from multiple classes.
+/// Note
+// Dart does not support multiple inheritance using classes. A class
+// cannot extend more than one class. However, Dart supports multiple
+// inheritance using mixins.
 
 /// Benefits of Inheritance
 // Code reusability.
-// Design program in a better way.
-// make code simple, clean and save time and money on maintenance.
+// Better program structure.
+// Clean and maintainable code.
+// Saves development and maintenance time.
