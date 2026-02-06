85aba6495fd8d99bba22fa75032f90c46af60ebc
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-06 10:01:09 +0530

    predefined_methods.dart: comments add.

--- DataTime/predefined_methods.dart
+++ DataTime/predefined_methods.dart
@@ -1,14 +1,14 @@
-/// Predefined methods of 'DateTime' class that returns bool value.
+/// Predefined DateTime methods that return bool values
 
-// isBefore(dateTime): return true if given date-time is before today date-time.
-// isAfter(dateTime): return true if given date-time is after today date-time.
-// isAtSameMovementAs(dateTime): return true if both dates is matched.
+// isBefore(dateTime other): return true if given date-time is before today date-time.
+// isAfter(dateTime other): return true if given date-time is after today date-time.
+// isAtSameMovementAs(dateTime other): return true if both dates is matched.
 
 void main() {
-  // get current date-time
+  // Get current date-time
   DateTime today = DateTime.now();
 
-  // get birthdayData from String to DateTime type
+  // Birthday converted from String to DateTime
   DateTime myBirthday = DateTime.parse("1999-02-06");
 
   if (myBirthday.isAtSameMomentAs(today)) {
