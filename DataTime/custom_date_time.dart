6fa94d666184d538fc0153fdcd9dea6ef5fb1b15
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-06 09:57:37 +0530

    custom_date_time.dart: comments add.

--- DataTime/custom_date_time.dart
+++ DataTime/custom_date_time.dart
@@ -1,11 +1,15 @@
 /// Convert custom String type date and time into DateTime type
-/// DateTime.parse(String) method is use to convert
+/// DateTime.parse(String) is used for conversion.
 
 
 void main(){
+  // Date in YYYY-MM-DD format
   String myBirthData = "1999-02-06"; // year-month-day
+
+  // Convert String to DateTime
   DateTime myBirthDate = DateTime.parse(myBirthData);
 
+  // Access DateTime properties
   print('Year: ${myBirthDate.year}'); // Year: 1999
   print('Month: ${myBirthDate.month}'); // Month: 2
   print('Day: '+myBirthDate.day.toString()); // Day: 6
